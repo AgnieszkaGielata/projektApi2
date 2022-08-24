69067d739a1da4c181c204006afffc214bdfdba9
Agnieszka Gielata <agielat@o2.pl>
2022-08-24 17:39:24 +0200

    first commit

---
+++ reservation-front/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Guests } from "./components/Guests";
+import {AddGuest} from "./components/AddGuest";
+
+function App() {
+  return (
+    <div className="App">
+      <AddGuest/>
+      <Guests/>
+    </div>
+  );
+}
+
+export default App;
